f32a8a494e623c2c2709950fec0d6c3aac342705
awilson42 <adam.wilson42@gmail.com>
2025-06-21 16:29:13 +0100

    update main.py

--- main.py
+++ main.py
@@ -3,13 +3,35 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while pygame.get_init() == True:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    AsteroidField()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        updatable.update(dt)
         screen.fill("black")
+        for pic in drawable:
+            pic.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
