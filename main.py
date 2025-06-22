212d473c7aa591ba1ab050dc2cc2377b27cf9460
awilson42 <adam.wilson42@gmail.com>
2025-06-22 14:04:31 +0100

    add main.py

--- main.py
+++ main.py
@@ -2,47 +2,9 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import sys
-from constants import *
-from circleshape import *
-from player import *
-from asteroid import *
-from asteroidfield import *
-import random
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    bullets = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    Bullet.containers = (bullets, updatable, drawable)
-    Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    AsteroidField()
-    clock = pygame.time.Clock()
-    dt = 0
+    print("Starting Asteroids!")
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        updatable.update(dt)
-        for asteroid in asteroids:
-            if asteroid.collision(player) == True:
-                sys.exit("Game Over!")
-            for bullet in bullets:
-                if asteroid.collision(bullet) == True:
-                    asteroid.split(asteroid.radius, asteroid.position, asteroid.velocity)
-                    bullet.kill()
-        screen.fill("black")
-        for pic in drawable:
-            pic.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
